b929473b6b7b955432cfe15d4132f77259197564
์์Nicharee Prombut <nicharee209@gmail.com>
2022-02-17 14:34:35 +0700

    homework

--- intro-to-js/nicharee.js
+++ intro-to-js/nicharee.js
@@ -1,3 +1,13 @@
-function toServe() {
-    alert('รอแปบ กำลังนำอาหารไปสั่งให้ครับ')
+function AskMyName() {
+    const name = prompt("Please enter your name", " Luknam");
+
+    if (name != null) {
+        document.getElementById("mynameis").innerHTML = 
+        "My name is" + name;
+}
+}
+
+function HelloWorld() {
+    const h1message = document.getElementById ("mynameis").innerHTML
+    alert(h1message + ' ข้าวมันไก่ หรือจะสู้ ข้าวมาใกล้ๆหน่อยอยากกอด')
 }
